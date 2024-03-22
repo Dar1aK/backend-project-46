d8be581a9a7f2e61c56bac2601705692a6e7157a
Daria Karataeva <daria.karataeva@yandex.ru>
2024-03-22 21:07:15 +0300

    add parse func

--- src/gendiff.js
+++ src/gendiff.js
@@ -2,30 +2,27 @@ import _ from "lodash";
 import path from "path";
 import fs from "fs";
 
-const getGenDiff = (filepath1, filepath2) => {
-  let path1;
-  let path2;
-
-  if (!filepath1.startsWith(".")) {
-    path1 = `${process.cwd()}${filepath1}`;
-  } else {
-    path1 = filepath1;
+const parseFile = (filepath) => {
+  if (!filepath.startsWith(".")) {
+    return `${process.cwd()}${filepath}`;
   }
+  return filepath;
+};
 
-  if (!filepath2.startsWith(".")) {
-    path2 = `${process.cwd()}${filepath2}`;
-  } else {
-    path2 = filepath2;
-  }
+const parseFiles = (filepath1, filepath2) => {
+  const path1 = parseFile(filepath1);
+  const path2 = parseFile(filepath2);
 
   const data1 = JSON.parse(fs.readFileSync(path.resolve(path1), "utf-8"));
   const data2 = JSON.parse(fs.readFileSync(path.resolve(path2), "utf-8"));
-
+  return { data1, data2 };
+};
+const getGenDiff = (filepath1, filepath2) => {
+  const { data1, data2 } = parseFiles(filepath1, filepath2);
   const result = {};
   const keys1 = Object.keys(data1);
   const keys2 = Object.keys(data2);
   const keys = _.union(keys1, keys2);
-
   for (const key of keys) {
     if (!Object.hasOwn(data1, key)) {
       result[`+ ${key}`] = data2[key];
@@ -38,7 +35,6 @@ const getGenDiff = (filepath1, filepath2) => {
       result[key] = data1[key];
     }
   }
-
   const sortedResult = _(result)
     .toPairs()
     .sortBy([
@@ -46,7 +42,6 @@ const getGenDiff = (filepath1, filepath2) => {
         const objectKeys = key.split(" ");
         const sortByKey = objectKeys[1] || objectKeys[0];
         const sortBySign = objectKeys[0] === "-" ? -1 : 1;
-
         return [sortByKey, sortBySign];
       },
     ])
