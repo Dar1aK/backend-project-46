bcabd96d60578380d460bec17392c66ff8942c8a
root <root@MSI>
2024-03-22 20:00:04 +0300

    gendiff compare objects

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 ### Hexlet tests and linter status:
+
 [![Actions Status](https://github.com/Dar1aK/backend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Dar1aK/backend-project-46/actions)
+
+### gendiff
+
+[![asciicast](https://asciinema.org/a/FVew4qwyJMUF9a00rxnGLTxML.svg)](https://asciinema.org/a/FVew4qwyJMUF9a00rxnGLTxML)
--- src/gendiff.js
+++ src/gendiff.js
@@ -1,22 +1,48 @@
-import { cwd } from "node:process";
-
-import * as path from "path";
-// const path = require("path");
+import _ from "lodash";
+import path from "path";
 import fs from "fs";
 
 export const getGenDiff = (filepath1, filepath2) => {
-  let path1 = "";
-  let path2 = "";
-  if (!filepath1.startsWith(".")) {
-    path1 = `${process.cwd()}${filepath1}`;
-  } else {
-    path1 = filepath1;
+  const path1 = !filepath1.startsWith(".")
+    ? `${process.cwd()}${filepath1}`
+    : filepath1;
+  const path2 = !filepath2.startsWith(".")
+    ? `${process.cwd()}${filepath2}`
+    : filepath2;
+
+  const data1 = JSON.parse(fs.readFileSync(path.resolve(path1), "utf-8"));
+  const data2 = JSON.parse(fs.readFileSync(path.resolve(path2), "utf-8"));
+
+  const result = {};
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+  const keys = _.union(keys1, keys2);
+
+  for (const key of keys) {
+    if (!Object.hasOwn(data1, key)) {
+      result[`+ ${key}`] = data2[key];
+    } else if (!Object.hasOwn(data2, key)) {
+      result[`- ${key}`] = data1[key];
+    } else if (data1[key] !== data2[key]) {
+      result[`- ${key}`] = data1[key];
+      result[`+ ${key}`] = data2[key];
+    } else {
+      result[key] = data1[key];
+    }
   }
-  if (!filepath2.startsWith(".")) {
-    path2 = `${process.cwd()}${filepath2}`;
-  } else {
-    path2 = filepath2;
-  }
-  const file1 = fs.readFileSync(path.resolve(path1), "utf-8");
-  const file2 = fs.readFileSync(path.resolve(path2), "utf-8");
+
+  const sortedResult = _(result)
+    .toPairs()
+    .sortBy([
+      function ([key]) {
+        const keys = key.split(" ");
+        const sortByKey = keys[1] || keys[0];
+        const sortBySign = keys[0] === "-" ? -1 : 1;
+
+        return [sortByKey, sortBySign];
+      },
+    ])
+    .fromPairs()
+    .value();
+  console.log(sortedResult);
 };
