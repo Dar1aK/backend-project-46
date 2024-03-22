9ea45ee80809c1446b92136682035f043d58e21a
Daria Karataeva <daria.karataeva@yandex.ru>
2024-03-22 22:49:49 +0300

    module path

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -4,22 +4,30 @@ import getGenDiff from "../src/gendiff.js";
 
 jest.useFakeTimers();
 
-test("getGenDiff", async () => {
-  expect(getGenDiff("./files/file1.json", "./files/file2.json")).toEqual({
-    "- follow": false,
-    host: "hexlet.io",
-    "- proxy": "123.234.53.22",
-    "- timeout": 50,
-    "+ timeout": 20,
-    "+ verbose": true,
+describe("getGenDiff", () => {
+  test("run with relative paths", async () => {
+    jest.useFakeTimers({ timerLimit: 5000 });
+
+    expect(getGenDiff("./files/file1.json", "./files/file2.json")).toEqual({
+      "- follow": false,
+      host: "hexlet.io",
+      "- proxy": "123.234.53.22",
+      "- timeout": 50,
+      "+ timeout": 20,
+      "+ verbose": true,
+    });
   });
 
-  expect(getGenDiff("/files/file1.json", "/files/file2.json")).toEqual({
-    "- follow": false,
-    host: "hexlet.io",
-    "- proxy": "123.234.53.22",
-    "- timeout": 50,
-    "+ timeout": 20,
-    "+ verbose": true,
+  test("run with absolute paths", async () => {
+    jest.useFakeTimers({ timerLimit: 5000 });
+
+    expect(getGenDiff("/files/file1.json", "/files/file2.json")).toEqual({
+      "- follow": false,
+      host: "hexlet.io",
+      "- proxy": "123.234.53.22",
+      "- timeout": 50,
+      "+ timeout": 20,
+      "+ verbose": true,
+    });
   });
 });
