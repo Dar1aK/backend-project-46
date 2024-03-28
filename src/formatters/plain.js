bbf1490caaf01c5dbc12758182174e7104ab673b
Daria Karataeva <daria.karataeva@yandex.ru>
2024-03-28 21:28:05 +0300

    beautify plain formatter

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -17,22 +17,53 @@ const printValue = (value) => {
   return value;
 };
 
+const getAction = (arr, i, arrKeys, val) => {
+  if (
+    arr[i + 1]?.[0].split(" ")[1] &&
+    arr[i + 1][0].split(" ")[1] === arrKeys[1]
+  ) {
+    return {
+      action: ACTIONS.updated,
+      newValue: arr[i + 1]?.[1],
+      oldValue: val,
+    };
+  }
+  if (arrKeys[0] === "+" && arr[i - 1]?.[0].split(" ")[1] !== arrKeys[1]) {
+    return {
+      action: ACTIONS.added,
+      newValue: val,
+    };
+  }
+  if (arrKeys[0] === "-") {
+    return {
+      action: ACTIONS.removed,
+    };
+  }
+  return {};
+};
+
+const combineLine = (actionObject, string) => {
+  if (actionObject.action === ACTIONS.added) {
+    return `Property '${string}' was added with value: ${printValue(actionObject.newValue)}\n`;
+  }
+  if (actionObject.action === ACTIONS.removed) {
+    return `Property '${string}' was removed\n`;
+  }
+  if (actionObject.action === ACTIONS.updated) {
+    return `Property '${string}' was updated. From ${printValue(actionObject.oldValue)} to ${printValue(actionObject.newValue)}\n`;
+  }
+  return "";
+};
+
 const plain = (tree) => {
   let result = "";
   const iter = (value, string, actionObject) => {
-    if (actionObject.action === ACTIONS.added) {
-      result += `Property '${string}' was added with value: ${printValue(actionObject.newValue)}\n`;
-    } else if (actionObject.action === ACTIONS.removed) {
-      result += `Property '${string}' was removed\n`;
-    } else if (actionObject.action === ACTIONS.updated) {
-      result += `Property '${string}' was updated. From ${printValue(actionObject.oldValue)} to ${printValue(actionObject.newValue)}\n`;
-    }
-
+    result += combineLine(actionObject, string);
     if (!value || typeof value !== "object") {
       return;
     }
 
-    const resultArray = Object.entries(value).map(([key, val], i, arr) => {
+    Object.entries(value).forEach(([key, val], i, arr) => {
       const isStartsWithSign = key.startsWith("+") || key.startsWith("-");
       const arrKeys = key.split(" ");
       const newKey = (() => {
@@ -43,33 +74,7 @@ const plain = (tree) => {
           (isStartsWithSign ? compareWithSign : defaultValue)
         );
       })();
-      const action = (() => {
-        if (
-          arr[i + 1]?.[0].split(" ")[1] &&
-          arr[i + 1][0].split(" ")[1] === arrKeys[1]
-        ) {
-          return {
-            action: ACTIONS.updated,
-            newValue: arr[i + 1]?.[1],
-            oldValue: val,
-          };
-        }
-        if (
-          arrKeys[0] === "+" &&
-          arr[i - 1]?.[0].split(" ")[1] !== arrKeys[1]
-        ) {
-          return {
-            action: ACTIONS.added,
-            newValue: val,
-          };
-        }
-        if (arrKeys[0] === "-") {
-          return {
-            action: ACTIONS.removed,
-          };
-        }
-        return {};
-      })();
+      const action = getAction(arr, i, arrKeys, val);
 
       return iter(val, `${string}${newKey}`, action);
     });
